df1cd32833cb4a81816a458d4556d555f4838c5a
Basson <bassoncoetzee7@gmail.com>
2022-11-05 18:41:17 +0200

    added IONIC app

--- be/FishPalAPI/FishPalAPI/Data/ApplicationDbContext.cs
+++ be/FishPalAPI/FishPalAPI/Data/ApplicationDbContext.cs
@@ -23,8 +23,8 @@ namespace FishPalAPI.Data
             if (!optionsBuilder.IsConfigured)
             {
                 //optionsBuilder.UseMySql("datasource = localhost; port = 7777; username = root; password = AWE7; database = fishPalDB", new MySqlServerVersion(new Version(8, 0, 28)));
-                //optionsBuilder.UseMySql("server=localhost;user id = root; Password=awe7;database=fishPalDB", new MySqlServerVersion(new Version(8, 0, 28)));
-                optionsBuilder.UseMySql("server=localhost;user id=root;Password=Ernst123?;database=fishPalDB", new MySqlServerVersion(new Version(8, 0, 28)));
+                optionsBuilder.UseMySql("server=localhost;user id = root; Password=awe7;database=fishPalDB", new MySqlServerVersion(new Version(8, 0, 28)));
+                //optionsBuilder.UseMySql("server=localhost;user id=root;Password=Ernst123?;database=fishPalDB", new MySqlServerVersion(new Version(8, 0, 28)));
             }
         }
 
